9851392120d02793b994738a2db3ed2d256d0e9c
Mirshad Oz <mirshadoz@Mirshads-MacBook-Pro.local>
2020-04-24 19:04:16 +0600

    Integrated sample data via seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+    title: "My Blog Post #{blog}",
+    body: "Cupcake ipsum dolor sit amet jujubes gingerbread lemon drops. Jelly-o I love candy canes liquorice danish I love macaroon soufflé caramels. Cheesecake fruitcake fruitcake chocolate cake candy canes I love wafer I love."
+  )
+end
+
+puts "10 blogs created successfully"
+
+5.times do |skill|
+  Skill.create!(
+    title: "Web development skill #{skill}",
+    percent_utilized: 99
+  )
+end
+
+puts "5 skills created successfully"
+
+9.times do |portfolio|
+  Portfolio.create!(
+    title: "Titlezzz #{portfolio}",
+    subtitle: "Test portfolio subtitle",
+    body: "Jelly-o gummi bears cookie. Dragée jujubes halvah gingerbread biscuit cake fruitcake bear claw gingerbread. Jelly biscuit marzipan icing donut I love chocolate bar tart.",    
+    main_image: "https://via.placeholder.com/600x400",
+    thumb_image: "https://via.placeholder.com/300x150"
+  )
+end
+
+
+puts "9 portfolio items created successfully"
