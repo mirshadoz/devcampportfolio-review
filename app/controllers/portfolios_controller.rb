7ca783ae1bd650512db321ab59d4df3dface31ab
Mirshad Oz <mirshadoz@Mirshads-MacBook-Pro.local>
2020-04-26 21:18:24 +0600

    Integrated Show action for Portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -33,8 +33,11 @@ class PortfoliosController < ApplicationController
       else
         format.html { render :edit }      
       end
-    end
-    
+    end    
+  end
+
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
   end
 
 
