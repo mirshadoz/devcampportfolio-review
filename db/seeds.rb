46c05061462c95417775e4418f641b3bbb51456f
Mirshad Oz <mirshadoz@Mirshads-MacBook-Pro.local>
2020-05-06 23:13:26 +0600

    Integrated custom scopes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+  )
+end
+
+puts "10 blogs created successfully"
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
-    body: "Cupcake ipsum dolor sit amet jujubes gingerbread lemon drops. Jelly-o I love candy canes liquorice danish I love macaroon soufflé caramels. Cheesecake fruitcake fruitcake chocolate cake candy canes I love wafer I love."
+    body: "Cupcake ipsum dolor sit amet jujubes gingerbread lemon drops. Jelly-o I love candy canes liquorice danish I love macaroon soufflé caramels. Cheesecake fruitcake fruitcake chocolate cake candy canes I love wafer I love.",
+    topic_id: Topic.last.id
+
   )
 end
 
@@ -16,10 +26,20 @@ end
 
 puts "5 skills created successfully"
 
-9.times do |portfolio|
+8.times do |portfolio|
   Portfolio.create!(
     title: "Titlezzz #{portfolio}",
-    subtitle: "Test portfolio subtitle",
+    subtitle: "Ruby on Rails",
+    body: "Jelly-o gummi bears cookie. Dragée jujubes halvah gingerbread biscuit cake fruitcake bear claw gingerbread. Jelly biscuit marzipan icing donut I love chocolate bar tart.",    
+    main_image: "https://via.placeholder.com/600x400",
+    thumb_image: "https://via.placeholder.com/300x150"
+  )
+end
+
+1.times do |portfolio|
+  Portfolio.create!(
+    title: "Titlezzz #{portfolio}",
+    subtitle: "Angular",
     body: "Jelly-o gummi bears cookie. Dragée jujubes halvah gingerbread biscuit cake fruitcake bear claw gingerbread. Jelly biscuit marzipan icing donut I love chocolate bar tart.",    
     main_image: "https://via.placeholder.com/600x400",
     thumb_image: "https://via.placeholder.com/300x150"
@@ -27,4 +47,4 @@ puts "5 skills created successfully"
 end
 
 
-puts "9 portfolio items created successfully"
+puts "1 portfolio items created successfully"
