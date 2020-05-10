ed6554479434b5823923e731607f6605a6f26320
Mirshad Oz <mirshadoz@Mirshads-MacBook-Pro.local>
2020-05-10 20:52:11 +0600

    Implement Portfolio defaults for portfolio image

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -14,13 +14,16 @@ class PortfoliosController < ApplicationController
   end
 
   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    puts "I am in CREATED METHOD"
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image))
 
     respond_to do |format|
       if @portfolio_item.save
+        puts "PORTFOLIO ITEM SAVED!!!"
         format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
       else
         format.html { render :new }
+        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
       end
     end
   end
